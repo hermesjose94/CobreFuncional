002131a1ee346948cbc7c4aa4719ed6f540631dd
hermesjose94 <hermesjose94@gmail.com>
2015-04-20 18:46:09 -0430

    asd2

--- expressHandlers/handlingRoutes.js
+++ expressHandlers/handlingRoutes.js
@@ -2,15 +2,15 @@
 module.exports = function(app){
 
     require('../ledControl/led.js')(app);
-    require('../js/init.js')(app);
-    require('../js/jquery-1.11.1.min.js')(app);
-    require('../js/jquery-2.1.1.min.js')(app);
-    require('../js/jquery.js')(app);
-    require('../js/jquery.timeago.min.js')(app);
-    require('../js/materialize.js')(app);
-    require('../js/materialize.min.js')(app);
-    require('../js/proceso.min.js')(app);
-    
+   // require('../js/init.js')(app);
+   // require('../js/jquery-1.11.1.min.js')(app);
+   // require('../js/jquery-2.1.1.min.js')(app);
+   // require('../js/jquery.js')(app);
+   // require('../js/jquery.timeago.min.js')(app);
+   // require('../js/materialize.js')(app);
+   // require('../js/materialize.min.js')(app);
+   // require('../js/proceso.min.js')(app);
+
     app.use(function(req, res, next){
     	res.status(404);
     	res.render('404');
