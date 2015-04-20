95fd72803bb83844d9166210e6c358e36c075cd8
hermesjose94 <hermesjose94@gmail.com>
2015-04-20 17:44:53 -0430

    subida 2

--- expressHandlers/handlingRoutes.js
+++ expressHandlers/handlingRoutes.js
@@ -1,23 +1,7 @@
 
 module.exports = function(app){
 
-    app.get('/', function(res, res){
-    	res.render('home');
-    });
-
-    app.get('/simulation', function(req, res){
-    	res.render('simulation');
-    });
-
-    app.get('/about', function(req, res){
-    	res.render('about');
-    });
-
-    app.get('/test', function(req, res){
-    	res.render('test');
-    });
-
-    require('../ledControl/led.js')(app);
+    //require('../ledControl/led.js')(app);
 
     app.use(function(req, res, next){
     	res.status(404);
